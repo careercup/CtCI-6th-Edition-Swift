f6236e18467b5c79c43e260e96b3b870ba19e5d1
Maksym Prokopchuk <rowwingman@gmail.com>
2018-01-25 00:10:54 +0200

    Refactor task 1.1 is Unique. Make code more readable

--- Ch 1. Arrays and Strings/Ch 1. Arrays and Strings.playground/Pages/1.1 Is Unique.xcplaygroundpage/Contents.swift
+++ Ch 1. Arrays and Strings/Ch 1. Arrays and Strings.playground/Pages/1.1 Is Unique.xcplaygroundpage/Contents.swift
@@ -1,30 +1,74 @@
 /*:
  1.1 Determine if a string has all unique characters. What if you can't use additional data structures?
+ Algoritms below works with unicodes.
  */
 extension String {
-    
+
+    /// Check if string consists of only unique characters.
+    /// * It's time complexity is O(N) and storage is O(N)
     func hasUniqueCharacters() -> Bool {
-        var uniqueCharacters = Set<Character>()
-        for c in characters {
-            guard !uniqueCharacters.contains(c) else { return false }
-            uniqueCharacters.insert(c)
+        var isUnique = true
+        var characters = Set<Character>()
+        for character in self {
+            guard !characters.contains(character) else {
+                isUnique = false
+                break
+            }
+            characters.insert(character)
         }
-        return true
+        return isUnique
     }
-    
+
+}
+
+extension String {
+
+    /// Check if string consists of only unique characters.
+    /// * It's time complexity is O(N^2) and storage is O(1)
     func hasUniqueCharactersWithoutAdditionalDataStructs() -> Bool {
-        if let _ = characters.sorted().successiveElements (where: { $0 == $1 }) {
+        if let _ = self.sorted().successiveElements (where: { $0 == $1 }) {
             return false
         }
         return true
     }
+
+
+    /// Check if string consists of only unique characters.
+    /// * The same limits and solution like in the book.
+    /// * It's time complexity is O(N^2) and storage is O(1)
+    func hasUniqueCharactersASCIIWithoutAdditionalDataStructs() -> Bool {
+        var isUnique = true
+        var checker: Int32 = 0
+        guard let asciiValueOfA = Character("a").unicodeScalars.first?.value else { return false }
+        for character in self.sorted() {
+            // if character is not ascii then false
+            guard let asciiValue = character.unicodeScalars.first?.value else {
+                isUnique = false
+                break
+            }
+
+            let numberInAlphabet = asciiValue - asciiValueOfA
+            guard checker & (1 << numberInAlphabet) == 0 else {
+                isUnique = false
+                break
+            }
+            checker |= (1 << numberInAlphabet)
+            print("\(String(checker, radix: 2))")
+        }
+
+        return isUnique
+    }
+
 }
 
-let unique = "abc"
-assert(unique.hasUniqueCharacters())
-let dup = "abca"
+// tests:
+// Test string with unique characters. Expected result is true.
+"abc".hasUniqueCharacters()
+"abc".hasUniqueCharactersWithoutAdditionalDataStructs()
+"abc".hasUniqueCharactersASCIIWithoutAdditionalDataStructs()
 
-assert(!dup.hasUniqueCharactersWithoutAdditionalDataStructs())
-
-
+// Test string with repeate characters. Expected result is false.
+"abca".hasUniqueCharacters()
+"abca".hasUniqueCharactersWithoutAdditionalDataStructs()
+"abca".hasUniqueCharactersASCIIWithoutAdditionalDataStructs()
 
