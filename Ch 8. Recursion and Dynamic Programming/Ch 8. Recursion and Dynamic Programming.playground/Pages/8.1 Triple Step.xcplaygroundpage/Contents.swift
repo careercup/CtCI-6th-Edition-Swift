364580ce4f5c9349bf1a2839ccdb71cda56be3fa
Ahmed Denison-Afifi <adenisonafifi@gmail.com>
2018-04-04 08:43:35 -0500

    Revise such that memo array is used

--- Ch 8. Recursion and Dynamic Programming/Ch 8. Recursion and Dynamic Programming.playground/Pages/8.1 Triple Step.xcplaygroundpage/Contents.swift
+++ Ch 8. Recursion and Dynamic Programming/Ch 8. Recursion and Dynamic Programming.playground/Pages/8.1 Triple Step.xcplaygroundpage/Contents.swift
@@ -5,24 +5,26 @@ import Foundation
  Count the number of ways the child can run up the stairs.
  */
 
-func countWays(steps: Int) -> Int {
-    let results = Array<Int?>(repeating: nil, count: steps + 1)
-    let result = countWays(steps: steps, results: results)
-    return result
-}
 
-private func countWays(steps: Int, results: [Int?]) -> Int {
-    guard steps > 0 else { return steps < 0 ? 0 : 1 }
-    
-    if let result = results[steps] {
+func countWays(steps: Int) -> Int {
+    var results = Array<Int?>(repeating: nil, count: steps + 1)
+
+    var countWays: ((Int) -> Int)!
+    countWays = { steps -> Int in
+      guard steps > 0 else { return steps < 0 ? 0 : 1 }
+
+      if let result = results[steps] {
         return result
+      }
+
+      let n1 = countWays(steps - 1)
+      let n2 = countWays(steps - 2)
+      let n3 = countWays(steps - 3)
+      results[steps] = n1 + n2 + n3
+      return results[steps]!
     }
-    var results = results
-    let n1 = countWays(steps: steps - 1, results: results)
-    let n2 = countWays(steps: steps - 2, results: results)
-    let n3 = countWays(steps: steps - 3, results: results)
-    results[steps] = n1 + n2 + n3
-    return results[steps]!
+
+    return countWays(steps)
 }
 
 
