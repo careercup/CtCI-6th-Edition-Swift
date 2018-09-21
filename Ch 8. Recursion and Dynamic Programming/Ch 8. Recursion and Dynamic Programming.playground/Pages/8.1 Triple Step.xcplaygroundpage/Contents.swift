3e68fb72bfd44f77c55df4d418296e91614727ee
chrisdunaetz <chris@dunaetz.com>
2018-09-21 00:21:44 -0700

    Make `results` an inout parameter

--- Ch 8. Recursion and Dynamic Programming/Ch 8. Recursion and Dynamic Programming.playground/Pages/8.1 Triple Step.xcplaygroundpage/Contents.swift
+++ Ch 8. Recursion and Dynamic Programming/Ch 8. Recursion and Dynamic Programming.playground/Pages/8.1 Triple Step.xcplaygroundpage/Contents.swift
@@ -6,21 +6,21 @@ import Foundation
  */
 
 func countWays(steps: Int) -> Int {
-    let results = Array<Int?>(repeating: nil, count: steps + 1)
-    let result = countWays(steps: steps, results: results)
+    var results = Array<Int?>(repeating: nil, count: steps + 1)
+    let result = countWays(steps: steps, results: &results)
     return result
 }
 
-private func countWays(steps: Int, results: [Int?]) -> Int {
+private func countWays(steps: Int, results: inout [Int?]) -> Int {
     guard steps > 0 else { return steps < 0 ? 0 : 1 }
     
     if let result = results[steps] {
         return result
     }
-    var results = results
-    let n1 = countWays(steps: steps - 1, results: results)
-    let n2 = countWays(steps: steps - 2, results: results)
-    let n3 = countWays(steps: steps - 3, results: results)
+    
+    let n1 = countWays(steps: steps - 1, results: &results)
+    let n2 = countWays(steps: steps - 2, results: &results)
+    let n3 = countWays(steps: steps - 3, results: &results)
     results[steps] = n1 + n2 + n3
     return results[steps]!
 }
